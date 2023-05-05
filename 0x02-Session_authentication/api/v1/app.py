7c6a0056d55fa71bc78b25e9a32751f01e20b82e
mathjken <ogujohnkennedy@gmail.com>
2023-05-05 01:12:18 +0000

    app

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -76,4 +76,4 @@ def authenticate_user():
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=
+    app.run(host=host, port=port)
